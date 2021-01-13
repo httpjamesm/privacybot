2c30bfee58354378aa77b965026ccffe7e58845c
james <james@james.james>
2021-01-13 15:36:47 -0500

    quick shortcuts

--- main.py
+++ main.py
@@ -19,6 +19,24 @@ initial_extensions = [
 ]
 
 bot = commands.Bot(command_prefix=prefix)
+@bot.event
+async def on_message(message):
+    splitted = list(message.content)
+    if (message.content.startswith('/') and splitted[1] != "/"):
+            await message.delete()
+            del splitted[0]
+            embed=discord.Embed(title=":clock1: Self-Destructing Message from " + str(message.author.name), description=''.join(splitted), color=0xff2600)
+            embed.set_footer(text="Lifespan: 10 seconds")
+            await message.channel.send(embed=embed,delete_after=10)
+    elif (message.content.startswith('//')):
+            await message.delete()
+            del splitted[:2]
+            embed=discord.Embed(title=":clock1: Self-Destructing Message from Anonymous", description=''.join(splitted), color=0xff2600)
+            embed.set_footer(text="Lifespan: 10 seconds")
+            await message.channel.send(embed=embed,delete_after=10)
+    else:
+        await bot.process_commands(message)
+
 
 @bot.command()
 @commands.cooldown(1,30,commands.BucketType.guild)
