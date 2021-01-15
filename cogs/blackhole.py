788ec220fe44fdd7c71ece29625249f320fdfc9d
james <james@james.james>
2021-01-14 20:45:35 -0500

    fix blackhole bugs

--- cogs/blackhole.py
+++ cogs/blackhole.py
@@ -8,9 +8,10 @@ class blackhole(commands.Cog):
     def __init__(self,bot):
         self.bot = bot
         # Re-define the bot object into the class.
-        
+
     mongoclient = pymongo.MongoClient(settings.mongo_url)# Connect to MongoDB Server
-    blackholecol = mongoclient["blackholedb"]["channels"]
+    blackholedb = mongoclient["blackholedb"]
+    blackholecol = blackholedb["channels"]
 
     @commands.command()
     @commands.has_permissions(manage_messages=True)
@@ -19,10 +20,10 @@ class blackhole(commands.Cog):
             # Update the existing blackhole channel entry
             self.blackholecol.update_many({"serverid":ctx.guild.id},{"$set":{"channelid":channel.id}})
             await ctx.send(":white_check_mark: Blackhole channel updated for **" + ctx.guild.name + "**.")
-            return
-        # If this is the server's first time blackhole setup, create a new entry.
-        self.blackholecol.insert_one({"serverid":ctx.guild.id,"channelid":channel.id})
-        await ctx.send(":white_check_mark: Blackhole channel set for **" + ctx.guild.name + "**.")
+        else:
+            # If this is the server's first time blackhole setup, create a new entry.
+            self.blackholecol.insert_one({"serverid":ctx.guild.id,"channelid":channel.id})
+            await ctx.send(":white_check_mark: Blackhole channel set for **" + ctx.guild.name + "**.")
 
 def setup(bot):
     bot.add_cog(blackhole(bot))
